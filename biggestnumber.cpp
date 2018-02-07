e8600962ec238e6432ea9c54a940d2abfa6b0b62
akalshamsi <akalshamsi@mail.csuchico.edu>
2018-02-06 18:13:34 -0800

    find the biggest number between two numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,12 +6,28 @@ using namespace std; //so we don't need the prefix std::
 int main()
 {
   
+int first;
+
+int second;
+
+int Biggest;
+
+
+
+
   cout<<"Please enter a whole number:\n";
   
+  cin>>first;
+  
   cout<<"Please enter another whole number:\n";
+cin>>second;
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+if(first > second){Biggest = first;}
+else {Biggest = second;}
+
+  cout<<"Of those two numbers, the biggest is: "<<Biggest<<endl;
+
+cout<<"Thank you for playing."<<endl;
 
   return 0;
 }
